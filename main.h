a1ea651b0c425fd8f623d6678ec2afccc2f2b809
Emmanuel Owusu-Larbi <eowusularbi@gmail.com>
2023-01-24 20:39:25 +0000

    Update main.h

--- main.h
+++ main.h
@@ -18,26 +18,18 @@
 #define S_LONG 2
 #define S_SHORT 1
 
+
 /**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
- */
-struct fmt
+* struct fmt - Struct op
+*
+* @fmt: The format.
+* @fn: The function associated.
+*/
+typedef struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-typedef struct fmt fmt_t;
+} fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
